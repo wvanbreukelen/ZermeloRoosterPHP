1bba26dfb7115e1205960b203dffbde75da4e4ec
W. van Breukelen <wvanbreukelen@users.noreply.github.com>
2015-05-19 16:47:39 +0200

    Fail

--- example.php
+++ example.php
@@ -5,8 +5,4 @@ require('Zermelo.php');
 
 // Create a new Zermelo instance
 $zermelo = new ZermeloAPI('YOURSCHOOL');
-<<<<<<< HEAD
-=======
 
-print_r($zermelo->getStudentGridAhead('YOURSTUDENTID'));
->>>>>>> 8e6115ad684865727530b3d6418b5f72b0f8f029
