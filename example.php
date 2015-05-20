9132d8bbb44826abd435923321ea999e5d99647c
W. van Breukelen <wvanbreukelen@users.noreply.github.com>
2015-05-20 21:03:07 +0200

    Added note

--- example.php
+++ example.php
@@ -6,3 +6,7 @@ require('Zermelo.php');
 // Create a new Zermelo instance
 $zermelo = new ZermeloAPI('YOURSCHOOL');
 
+// NOTE
+
+"https://SCHOOLNAAM.zportal.nl/api/v2/appointments?user=~me&start=TIMESTAMP_BEGIN&end=TIMESTAMP_EINDE2&access_token=TOKEN"
+
