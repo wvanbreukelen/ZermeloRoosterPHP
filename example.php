154e2be1cd5de901ed2572240562915d851e9868
wvanbreukelen <wdownload@xs4all.nl>
2015-05-24 15:56:04 +0200

    Removed note

--- example.php
+++ example.php
@@ -5,12 +5,3 @@ require('Zermelo.php');
 
 // Create a new Zermelo instance
 $zermelo = new ZermeloAPI('YOURSCHOOL');
-<<<<<<< HEAD
-   
-=======
-
-// NOTE
-
-"https://SCHOOLNAAM.zportal.nl/api/v2/appointments?user=~me&start=TIMESTAMP_BEGIN&end=TIMESTAMP_EINDE2&access_token=TOKEN"
-
->>>>>>> origin/master
