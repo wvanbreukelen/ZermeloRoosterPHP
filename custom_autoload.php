243c8a44bad9a8b6f993f63b77cee30ae3409b49
W. van Breukelen <wvanbreukelen@users.noreply.github.com>
2015-06-23 20:36:21 +0200

    Moved register code into register_api function
    It is possible that the developer uses the composer autoloader. When using composer, the custom autoloader may be loaded too, so the API files are loaded twice. To make a workaround for this problem, the developer now simply uses the register_api function for including the API files.

--- custom_autoload.php
+++ custom_autoload.php
@@ -1,16 +1,18 @@
 <?php
 
-if (file_exists('Cache.json'))
+function register_api()
 {
-	include_once('Cache.php');
-} else {
-	throw new Exception("Cache.php has not been found!");
+	if (file_exists('Cache.json'))
+	{
+		include_once('Cache.php');
+	} else {
+		throw new Exception("Cache.php has not been found!");
+	}
+	
+	if (file_exists('Zermelo.php'))
+	{
+		include_once('Zermelo.php');
+	} else {
+		throw new Exception("Zermelo.php has not been found!");
+	}	
 }
-
-if (file_exists('Zermelo.php'))
-{
-	include_once('Zermelo.php');
-} else {
-	throw new Exception("Zermelo.php has not been found!");
-}
-
