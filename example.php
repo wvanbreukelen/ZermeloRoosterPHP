5932a9537c2e8298894e7caabc4216fad7496e44
W. van Breukelen <wvanbreukelen@users.noreply.github.com>
2015-09-15 16:29:11 +0200

    Changed example.php

--- example.php
+++ example.php
@@ -1,8 +1,12 @@
 <?php
 
-// Including the Zermelo API PHP
-require('Cache.php');
-require('Zermelo.php');
+// Including the Zermelo API PHP using Composer
+//require('vendor/autoload.php');
+
+// Including the Zermelo API PHP using the build-in autoloader
+require('custom_autoload.php')
+
+register_zermelo_api();
 
 // Create a new Zermelo instance
 $zermelo = new ZermeloAPI('YOURSCHOOL');
